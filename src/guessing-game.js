9882c652cd7e3d16733cd3542fd01610bfe7a7db
siarhei.nikitsin <siarhei.nikitsin@greyloud.com>
2019-10-07 16:56:11 +0300

    feat: implement algorithm

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,31 @@
 class GuessingGame {
-    constructor() {}
+    min = null;
+    max = null;
+    guessedNumber = null;
+    
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.guessedNumber = null;
     }
 
     guess() {
-
+        return this.reduceRange();
     }
 
     lower() {
-
+        this.max = this.reduceRange();
     }
 
     greater() {
+        this.min = this.reduceRange();
+    }
 
+    reduceRange(){
+        return Math.ceil((this.min + this.max) / 2);
     }
 }
 
